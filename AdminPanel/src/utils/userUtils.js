efbb67548fcca78e7947822b482f06cd1cb1adf3
Great Nwamadi <69170132+Great-NGO@users.noreply.github.com>
2025-04-23 12:56:25 +0100

    Switched to nullish coalescing operator for getDisplayName function

--- AdminPanel/src/utils/userUtils.js
+++ AdminPanel/src/utils/userUtils.js
@@ -1,8 +1,8 @@
 /** getDisplayName - takes in a user object and returns a users fullname */
 export const getDisplayName = (user) => {
     if(user?.name) return user.name;
-    const firstName = user?.firstName || "";
-    const lastName = user?.lastName || "";
+    const firstName = user?.firstName ?? "";
+    const lastName = user?.lastName ?? "";
 
     const fullName = `${firstName} ${lastName}`.trim();
 
